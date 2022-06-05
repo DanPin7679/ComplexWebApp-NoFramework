15c5391b9e99abf2f4930c388ddaf72beb0faad4
DanPin <dany.pineault@videotron.ca>
2022-06-05 15:59:46 -0400

    release 6

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ const pageMenu = [
 
 const mainContent = "Main Content Placeholder";
 
+document.body.classList.add("dark-mode");
+
+document.getElementById("theme-btn").addEventListener("click", () => {
+  console.log(document.body.classList);
+  if (document.body.classList == "light-mode") {
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+  }
+});
+
 NavBar(pageMenu);
 MainContent(mainContent);
 fetchLearningContent(Sidebar);
--- components/sidebar.js
+++ components/sidebar.js
@@ -1,14 +1,17 @@
 export const Sidebar = (moduleMenu) => {
-  var sideBarMenu = document.getElementById("sidebar-menu");
+  const sideBarMenu = document.getElementById("sidebar-menu");
+
   moduleMenu.map((item) => {
     let newItem = document.createElement("a");
-    newItem.className = "modules";
+    newItem.className = "sidebar-menu-main";
     newItem.id = item.id;
     newItem.innerHTML = item.name;
     sideBarMenu.appendChild(newItem);
 
     item.units.map((unit) => {
-      let newUnit = document.createElement("p");
+      let newUnit = document.createElement("a");
+      newUnit.className = "sidebar-menu-sub";
+      newUnit.id = unit.id;
       newUnit.innerHTML = unit.name;
       sideBarMenu.appendChild(newUnit);
     });
